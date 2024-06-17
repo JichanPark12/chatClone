c8b33010189df132a7195e40a541291298c77124
pjc0179 <pjc0179@gmail.com>
2024-06-17 18:21:16 +0900

    style:"chat header fixed 추가"

--- src/components/chatRoom/ChatRoomHeader.tsx
+++ src/components/chatRoom/ChatRoomHeader.tsx
@@ -5,11 +5,10 @@ import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 const ChatRoomHeader = () => {
   return (
-    <header>
-      <div className="p-3 flex items-center justify-between">
+    <header className="fixed w-full z-10 bg-chatRoom">
+      <div className="p-3 flex items-center justify-between ">
         <div className="flex">
           <ProfileIcon imageUrl={'/chatRoomImg.jpg'}></ProfileIcon>
-
           <div className="flex flex-col ml-3">
             <p className="">할일 없는 백수들 모임</p>
             <span className=" text-gray-500 text-xs">접속인원: 2명</span>
